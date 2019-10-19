7cb3705470dc1caa1cb7a419e2b6c810dc9eec88
Mikhail Sosnin <m.i.sosnin@mail.ru>
2019-10-19 06:54:56 -0500

    create upload_to_env.sh script

--- scripts/upload_to_env.sh
+++ scripts/upload_to_env.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+# copy runjob.sh
 scp src/main/resources/runjob.sh uncommon-gull@18.194.143.9:/home/uncommon-gull/big_data_spark_workspace
+
+# copy jar file
+scp target/etl-1.0-SNAPSHOT.jar uncommon-gull@18.194.143.9:/home/uncommon-gull/big_data_spark_workspace

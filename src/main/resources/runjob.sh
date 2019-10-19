76bf07b99fc6a8c88761ef4e3486d9c0368303c5
Mikhail Sosnin <m.i.sosnin@mail.ru>
2019-10-19 06:30:58 -0500

    update runjob.conf

--- src/main/resources/runjob.sh
+++ src/main/resources/runjob.sh
@@ -13,6 +13,6 @@ spark-submit                                    \
     etl-1.0-SNAPSHOT.jar                        \
     -t s3a://nonauth-common-friendsday-noqzfl/users_subscriptions_data_2019_sample25  \
     -m mongodb://ec2-3-120-132-157.eu-central-1.compute.amazonaws.com/vk_db.user_info_sample25            \
-    -s s3a://XXX/user_subscriptions_archive     \
-    -i s3a://XXX/user_info_archive              \
-    -d s3a://XXX/starspace_output
+    -s s3a://nonauth-friendsday-uncommon-gull/user_subscriptions_archive     \
+    -i s3a://nonauth-friendsday-uncommon-gull/user_info_archive              \
+    -d s3a://nonauth-friendsday-uncommon-gull/starspace_output

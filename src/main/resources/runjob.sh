be933e4a35cbaac2cd4213b4d9124159973eba85
Mikhail Sosnin <m.i.sosnin@mail.ru>
2019-10-19 06:18:47 -0500

    update runjob.conf

--- src/main/resources/runjob.sh
+++ src/main/resources/runjob.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 export SPARK_MAJOR_VERSION=2
 export HADOOP_USER_NAME=hdfs
-# TODO: заполнить ??? необходимой конфигурацией и XXX uri MongoDB и путями к S3 buckets
 spark-submit                                    \
     --name bdfd-etl                             \
     --class com.epam.bdfd.Runner                \
-    --master ???                                \
-    --deploy-mode ???                           \
-    --executor-memory ???                       \
-    --executor-cores ???                        \
-    --num-executors ???                         \
-    --driver-memory ???                         \
+    --master yarn                                \
+    --deploy-mode cluster                           \
+    --executor-memory 4G                       \
+    --executor-cores 2                        \
+    --num-executors 6                         \
+    --driver-memory 4G                         \
     etl-1.0-SNAPSHOT.jar                        \
-    -t s3a://XXX/users_subscriptions_data_2019  \
-    -m mongodb://XXX/vk_db.user_info            \
+    -t s3a://nonauth-common-friendsday-noqzfl/users_subscriptions_data_2019_sample  \
+    -m mongodb://ec2-3-120-132-157.eu-central-1.compute.amazonaws.com/vk_db.user_info            \
     -s s3a://XXX/user_subscriptions_archive     \
     -i s3a://XXX/user_info_archive              \
     -d s3a://XXX/starspace_output
